af0fa646f16fd4a944c5fcb512139f73227aa736
Matthew Lapworth <matthewl@bit-shift.net>
2016-08-29 21:05:06 -0700

    Output some debugging information
    Signed-off-by: Matthew Lapworth <matthewl@bit-shift.net>

--- dns-update.py
+++ dns-update.py
@@ -2,4 +2,5 @@ import httplib2
 
 h = httplib2.Http(".cache")
 (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
+print ("Response Headers: ", resp_headers)
 print ("Response JSON: ", content)
