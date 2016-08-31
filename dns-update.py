7b344bd9871d797a8a166e8bf6bf7b591a3ff85c
Matthew Lapworth <matthewl@bit-shift.net>
2016-08-30 21:17:51 -0700

    Figured out format of response content.
    Parsed response json and extracted IP address.
    
    Signed-off-by: Matthew Lapworth <matthewl@bit-shift.net>

--- dns-update.py
+++ dns-update.py
@@ -1,6 +1,14 @@
 import httplib2
+import json
 
 h = httplib2.Http(".cache")
 (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
 print ("Response Headers: ", resp_headers)
 print ("Response JSON: ", content)
+if type(content) is bytes:
+    print ("Type is Bytes")
+
+content_str = content.decode('utf-8')
+
+resp_json = json.loads(content_str)
+print ("IP Address: %s" % resp_json["ip"])
