6c31fdea76c7475c947859c0646e715f63dbbf3e
Matthew Lapworth <mlapworth@newrelic.com>
2016-09-12 21:29:32 -0700

    Add basic logging support
    Refactored IP retrieval code into a method.
    
    Signed-off-by: Matthew Lapworth <mlapworth@newrelic.com>

--- dns-update.py
+++ dns-update.py
@@ -1,14 +1,24 @@
 import httplib2
 import json
+import logging
 
-h = httplib2.Http(".cache")
-(resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
-print ("Response Headers: ", resp_headers)
-print ("Response JSON: ", content)
-if type(content) is bytes:
-    print ("Type is Bytes")
+def retrieve_ip_address():
+    h = httplib2.Http(".cache")
+    (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
+    logging.debug("Response Headers: %s" % resp_headers)
+    logging.debug("Response JSON: %s" % content)
+    if type(content) is bytes:
+        logging.debug("Type is Bytes")
 
-content_str = content.decode('utf-8')
+    content_str = content.decode('utf-8')
 
-resp_json = json.loads(content_str)
-print ("IP Address: %s" % resp_json["ip"])
+    resp_json = json.loads(content_str)
+    return resp_json["ip"]
+
+def create_logger():
+    logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+if __name__ == '__main__':
+    create_logger()
+    ext_ip = retrieve_ip_address()
+    logging.info("External IP Address is %s" % ext_ip)
