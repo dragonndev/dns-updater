7a5d1b32f4dcd4974ab7de34113fff5fb1948137
Matthew Lapworth <matthewl@bit-shift.net>
2016-09-18 13:55:40 -0700

    Ignoring PDF files Added method to build JSON to update/create DNS name Added string constants w/ URLs for dns host
    Signed-off-by: Matthew Lapworth <matthewl@bit-shift.net>

--- dns-update.py
+++ dns-update.py
@@ -2,12 +2,27 @@ import httplib2
 import json
 import logging
 
-DYN_HOST_NAME = "mobile.bit-shift.net"
-DNS_REGISTRAR_API_URL = ""
+DYN_HOST_NAME = "mobile"
+DOMAIN_NAME = "bit-shift.net"
+DNS_REGISTRAR_API_URL = "https://api.dev.name.com/"
+DNS_DOMAIN_CREATE_URL = "api/dns/create/"
+
+def create_dns_update_payload(ip_address):
+    create_json = {
+        "hostname" : DYN_HOST_NAME,
+        "type" : "A",
+        "content" : ext_ip,
+        "ttl" : 86400,
+        "priority" : 10
+    }
+
+    return create_json
 
 def set_new_ip(ip_address):
+    create_domain_json = create_dns_update_payload(ip_address)
     h = httplib2.Http(".cache")
-    (resp_headers, content) = h.request(DNS_REGISTRAR_API_URL, "POST", body="", headers={})
+    create_domain_URL = "%s%s" % DNS_REGISTRAR_API_URL, DNS_DOMAIN_CREATE_URL
+    (resp_headers, content) = h.request(create_domain_URL, "POST", body=create_domain_json, headers={})
 
 def retrieve_ip_address():
     h = httplib2.Http(".cache")
