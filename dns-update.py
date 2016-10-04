c895a205c941276a8bd6a9e5120dfa00d49b68ce
Matthew Lapworth <mlapworth@newrelic.com>
2016-10-04 12:11:36 -0700

    Switch DNS provider to freeDNS from Name.com. Add code to read API key from ENV
    Signed-off-by: Matthew Lapworth <mlapworth@newrelic.com>

--- dns-update.py
+++ dns-update.py
@@ -1,28 +1,22 @@
 import httplib2
 import json
 import logging
+import os
 
-DYN_HOST_NAME = "mobile"
-DOMAIN_NAME = "bit-shift.net"
-DNS_REGISTRAR_API_URL = "https://api.dev.name.com/"
-DNS_DOMAIN_CREATE_URL = "api/dns/create/"
+DYN_HOST_NAME = "mytestdomain.strangled.net"
+DNS_REGISTRAR_API_URL = "https://sync.afraid.org/u/%s/?hostname=%s&myip=%s&content-type=json"
+DOMAIN_API_KEY = ""
 
-def create_dns_update_payload(ip_address):
-    create_json = {
-        "hostname" : DYN_HOST_NAME,
-        "type" : "A",
-        "content" : ext_ip,
-        "ttl" : 86400,
-        "priority" : 10
-    }
-
-    return create_json
+def create_dns_update_url(ip_address):
+    update_url = DNS_REGISTRAR_API_URL % DOMAIN_API_KEY, DYN_HOST_NAME, ip_address 
+    logging.debug("Update URL: %s" % update_url)
+    return update_url
 
 def set_new_ip(ip_address):
-    create_domain_json = create_dns_update_payload(ip_address)
+    dynamic_dns_update_url = create_dns_update_url(ip_address)
     h = httplib2.Http(".cache")
-    create_domain_URL = "%s%s" % DNS_REGISTRAR_API_URL, DNS_DOMAIN_CREATE_URL
-    (resp_headers, content) = h.request(create_domain_URL, "POST", body=create_domain_json, headers={})
+    (resp_headers, content) = h.request(dynamic_dns_update_url, "GET")
+    logging.debug("Response: %s", content)
 
 def retrieve_ip_address():
     h = httplib2.Http(".cache")
@@ -37,11 +31,19 @@ def retrieve_ip_address():
     resp_json = json.loads(content_str)
     return resp_json["ip"]
 
+def load_environment():
+    DOMAIN_API_KEY = os.getenv('FREE_DNS_TOKEN')
+    if not DOMAIN_API_KEY:
+        logging.error("Dynamic DNS token not set in environment!")
+    else:
+        logging.debug("Dynamic DNS token retrieved from environment")
+
 def create_logger():
     logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 if __name__ == '__main__':
     create_logger()
+    load_environment()
     ext_ip = retrieve_ip_address()
     set_new_ip(ext_ip)
     logging.info("External IP Address is %s" % ext_ip)
