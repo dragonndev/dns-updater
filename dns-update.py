44e29e0a5de77734834ef0fd331b88c7e58ec1c6
Matthew Lapworth <mlapworth@newrelic.com>
2016-11-01 11:16:46 -0700

    First functionally complete version. Removed global variable for holding DNS update API token
    Signed-off-by: Matthew Lapworth <mlapworth@newrelic.com>

--- dns-update.py
+++ dns-update.py
@@ -5,15 +5,14 @@ import os
 
 DYN_HOST_NAME = "mytestdomain.strangled.net"
 DNS_REGISTRAR_API_URL = "https://sync.afraid.org/u/%s/?hostname=%s&myip=%s&content-type=json"
-DOMAIN_API_KEY = ""
 
-def create_dns_update_url(ip_address):
-    update_url = DNS_REGISTRAR_API_URL % DOMAIN_API_KEY, DYN_HOST_NAME, ip_address 
+def create_dns_update_url(domain_api_key, ip_address):
+    update_url = DNS_REGISTRAR_API_URL % (domain_api_key, DYN_HOST_NAME, ip_address) 
     logging.debug("Update URL: %s" % update_url)
     return update_url
 
-def set_new_ip(ip_address):
-    dynamic_dns_update_url = create_dns_update_url(ip_address)
+def set_new_ip(domain_api_key, ip_address):
+    dynamic_dns_update_url = create_dns_update_url(domain_api_key, ip_address)
     h = httplib2.Http(".cache")
     (resp_headers, content) = h.request(dynamic_dns_update_url, "GET")
     logging.debug("Response: %s", content)
@@ -31,19 +30,20 @@ def retrieve_ip_address():
     resp_json = json.loads(content_str)
     return resp_json["ip"]
 
-def load_environment():
-    DOMAIN_API_KEY = os.getenv('FREE_DNS_TOKEN')
-    if not DOMAIN_API_KEY:
+def load_dns_api_key_from_environment():
+    domain_api_key = os.getenv('FREE_DNS_TOKEN')
+    if not domain_api_key:
         logging.error("Dynamic DNS token not set in environment!")
     else:
         logging.debug("Dynamic DNS token retrieved from environment")
+    return domain_api_key
 
 def create_logger():
     logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 if __name__ == '__main__':
     create_logger()
-    load_environment()
+    domain_api_key = load_dns_api_key_from_environment()
     ext_ip = retrieve_ip_address()
-    set_new_ip(ext_ip)
+    set_new_ip(domain_api_key, ext_ip)
     logging.info("External IP Address is %s" % ext_ip)
