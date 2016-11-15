fc92f4616f808f0b814e9bee730c04915e726f77
Matthew Lapworth <mlapworth@newrelic.com>
2016-11-15 11:49:14 -0800

    Converted methods to live within a DNS updater class. Modifying program syntax/flow to be more OO like.
    Signed-off-by: Matthew Lapworth <mlapworth@newrelic.com>

--- dns-update.py
+++ dns-update.py
@@ -3,47 +3,55 @@ import json
 import logging
 import os
 
-DYN_HOST_NAME = "mytestdomain.strangled.net"
-DNS_REGISTRAR_API_URL = "https://sync.afraid.org/u/%s/?hostname=%s&myip=%s&content-type=json"
+class DNSUpdater:
+    '''Class to update IP address information for a dynamic DNS hosting service'''
+    dyn_host_name = "mytestdomain.strangled.net"
+    dns_registration_api_url = "https://sync.afraid.org/u/%s/?hostname=%s&myip=%s&content-type=json"
 
-def create_dns_update_url(domain_api_key, ip_address):
-    update_url = DNS_REGISTRAR_API_URL % (domain_api_key, DYN_HOST_NAME, ip_address) 
-    logging.debug("Update URL: %s" % update_url)
-    return update_url
+    def __init__(self):
+        self.create_logger()
+        self.domain_api_key = self.load_dns_api_key_from_environment()
+        pass
 
-def set_new_ip(domain_api_key, ip_address):
-    dynamic_dns_update_url = create_dns_update_url(domain_api_key, ip_address)
-    h = httplib2.Http(".cache")
-    (resp_headers, content) = h.request(dynamic_dns_update_url, "GET")
-    logging.debug("Response: %s", content)
+    def update_dyn_dns_setting(self):
+        ext_ip = self.retrieve_ip_address()
+        self.set_new_ip(ext_ip)
+        logging.info("External IP Address is %s" % ext_ip)
+    
+    def create_dns_update_url(self, ip_address):
+        update_url = self.dns_registration_api_url % (self.domain_api_key, self.dyn_host_name, ip_address) 
+        logging.debug("Update URL: %s" % update_url)
+        return update_url
 
-def retrieve_ip_address():
-    h = httplib2.Http(".cache")
-    (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
-    logging.debug("Response Headers: %s" % resp_headers)
-    logging.debug("Response JSON: %s" % content)
-    if type(content) is bytes:
-        logging.debug("Type is Bytes")
+    def set_new_ip(self, ip_address):
+        dynamic_dns_update_url = self.create_dns_update_url(ip_address)
+        h = httplib2.Http(".cache")
+        (resp_headers, content) = h.request(dynamic_dns_update_url, "GET")
+        logging.debug("Response: %s", content)
 
-    content_str = content.decode('utf-8')
+    def retrieve_ip_address(self):
+        h = httplib2.Http(".cache")
+        (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
+        logging.debug("Response Headers: %s" % resp_headers)
+        logging.debug("Response JSON: %s" % content)
+        if type(content) is bytes:
+            logging.debug("Type is Bytes")
 
-    resp_json = json.loads(content_str)
-    return resp_json["ip"]
+        content_str = content.decode('utf-8')
 
-def load_dns_api_key_from_environment():
-    domain_api_key = os.getenv('FREE_DNS_TOKEN')
-    if not domain_api_key:
-        logging.error("Dynamic DNS token not set in environment!")
-    else:
-        logging.debug("Dynamic DNS token retrieved from environment")
-    return domain_api_key
+        resp_json = json.loads(content_str)
+        return resp_json["ip"]
 
-def create_logger():
-    logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+    def load_dns_api_key_from_environment(self):
+        domain_api_key = os.getenv('FREE_DNS_TOKEN')
+        if not domain_api_key:
+            logging.error("Dynamic DNS token not set in environment!")
+        else:
+            logging.debug("Dynamic DNS token retrieved from environment")
+        return domain_api_key
 
-if __name__ == '__main__':
-    create_logger()
-    domain_api_key = load_dns_api_key_from_environment()
-    ext_ip = retrieve_ip_address()
-    set_new_ip(domain_api_key, ext_ip)
-    logging.info("External IP Address is %s" % ext_ip)
+    def create_logger(self):
+        logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+dns_update = DNSUpdater()
+dns_update.update_dyn_dns_setting()
