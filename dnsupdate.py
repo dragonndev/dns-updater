6f94e48468a1df24f9d9926c94bb9652f68699b2
Matthew Lapworth <mlapworth@newrelic.com>
2017-03-07 10:50:00 -0800

    Renamed main module Fixed pylint warnings
    Signed-off-by: Matthew Lapworth <mlapworth@newrelic.com>

--- dnsupdate.py
+++ dnsupdate.py
@@ -1,9 +1,9 @@
-import httplib2
 import json
 import logging
 import os
+import httplib2
 
-class DNSUpdater:
+class DNSUpdater(object):
     '''Class to update IP address information for a dynamic DNS hosting service'''
     dyn_host_name = "mytestdomain.strangled.net"
     dns_registration_api_url = "https://sync.afraid.org/u/%s/?hostname=%s&myip=%s&content-type=json"
@@ -16,25 +16,26 @@ class DNSUpdater:
     def update_dyn_dns_setting(self):
         ext_ip = self.retrieve_ip_address()
         self.set_new_ip(ext_ip)
-        logging.info("External IP Address is %s" % ext_ip)
-    
+        logging.info("External IP Address is %s", ext_ip)
+
     def create_dns_update_url(self, ip_address):
-        update_url = self.dns_registration_api_url % (self.domain_api_key, self.dyn_host_name, ip_address) 
-        logging.debug("Update URL: %s" % update_url)
+        update_url = self.dns_registration_api_url % (self.domain_api_key, self.dyn_host_name, ip_address)
+        logging.debug("Update URL: %s", update_url)
         return update_url
 
     def set_new_ip(self, ip_address):
         dynamic_dns_update_url = self.create_dns_update_url(ip_address)
-        h = httplib2.Http(".cache")
-        (resp_headers, content) = h.request(dynamic_dns_update_url, "GET")
+        h_client = httplib2.Http(".cache")
+        (resp_headers, content) = h_client.request(dynamic_dns_update_url, "GET")
+        logging.debug("Response headers: %s ", resp_headers)
         logging.debug("Response: %s", content)
 
     def retrieve_ip_address(self):
-        h = httplib2.Http(".cache")
-        (resp_headers, content) = h.request("https://api.ipify.org?format=json", "GET")
-        logging.debug("Response Headers: %s" % resp_headers)
-        logging.debug("Response JSON: %s" % content)
-        if type(content) is bytes:
+        h_client = httplib2.Http(".cache")
+        (resp_headers, content) = h_client.request("https://api.ipify.org?format=json", "GET")
+        logging.debug("Response Headers: %s", resp_headers)
+        logging.debug("Response JSON: %s", content)
+        if isinstance(content) is bytes:
             logging.debug("Type is Bytes")
 
         content_str = content.decode('utf-8')
@@ -53,5 +54,5 @@ class DNSUpdater:
     def create_logger(self):
         logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-dns_update = DNSUpdater()
-dns_update.update_dyn_dns_setting()
+DNS_UPDATE = DNSUpdater()
+DNS_UPDATE.update_dyn_dns_setting()
