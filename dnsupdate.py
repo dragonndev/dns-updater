1c9f6312a24c2d4196f40a4728fee2ec6ae068d3
Matthew Lapworth <matthewl@bit-shift.net>
2018-05-19 15:32:04 -0700

    Configuration Refactor
    Updated loading of API key and dns hostname from config file or
    environment.
    Simplified config entry name.

--- dnsupdate.py
+++ dnsupdate.py
@@ -7,12 +7,13 @@ class DNSUpdater(object):
 
     """Class to update IP address information for a dynamic DNS hosting service"""
     
-    dyn_host_name = "mytestdomain.strangled.net"
     dns_registration_api_url = "https://sync.afraid.org/u/%s/?myip=%s&content-type=json"
 
     def __init__(self):
         self.create_logger()
-        self.domain_api_key = self.load_dns_api_key_from_file()
+        self.app_config = self.load_configuration_from_file()
+        self.dns_api_token = self.load_dns_api_token()
+        self.dns_hostname = self.load_dns_hostname()
 
     def update_dyn_dns_setting(self):
         ext_ip = self.retrieve_ip_address()
@@ -20,7 +21,7 @@ class DNSUpdater(object):
         logging.info("External IP Address is %s", ext_ip)
 
     def create_dns_update_url(self, ip_address):
-        update_url = self.dns_registration_api_url % (self.domain_api_key, ip_address)
+        update_url = self.dns_registration_api_url % (self.dns_api_token, ip_address)
         logging.debug("Update URL: %s", update_url)
         return update_url
 
@@ -44,17 +45,47 @@ class DNSUpdater(object):
         resp_json = json.loads(content_str)
         return resp_json["ip"]
 
-    def load_dns_api_key_from_file(self):
-        try:
-        	credentials = json.load('credentials.json')
-        except Exception as e:
-        		logging.error('Credentials file not found. Error: {}'.format(e))
-        domain_api_key = credentials['dns_api_token']
+    def load_dns_hostname(self):
+    	if self.app_config is None:
+    		dns_hostname = os.getenv("DNS_HOSTNAME")
+    		if dns_hostname is None:
+    			logging.debug("DNS hostname not set in environment.")
+    			raise AttributeError("Cannot load DNS hostname.")
+    		logging.debug("DNS hostname: {} loaded from environment.".format(dns_hostname))
+    	else:
+    		dns_hostname = self.app_config["dns_hostname"]
+    		if dns_hostname is None:
+    			logging.debug("Cannot load DNS hostname from configuration file.")
+    			raise AttributeError("Cannot load DNS hostname.")
+    		logging.debug("DNS name: {}, loaded from configuration file.".format(dns_hostname))
+    	
+    	return dns_hostname    
+    
+    def load_dns_api_token(self):
+    	if self.app_config is None:
+    		dns_api_token = os.getenv("FREE_DNS_API_TOKEN")
+    		if dns_api_token is None:
+    			logging.debug("DNS API token not set in environment.")
+    			raise AttributeError("Cannot load DNS API token.")
+    		logging.debug("Dynamic DNS API token loaded from environment")
+    	else:
+    		dns_api_token = self.app_config["dns_api_token"]
+    		if dns_api_token is None:
+    			logging.debug("Cannot load DNS API token from configuration file.")
+    			raise AttributeError("Cannot load dynamic DNS API token.")
+    		logging.debug("Dynamic DNS API token loaded from configuration file")
+    			
+			return dns_api_token
         
-        logging.debug("Dynamic DNS token retrieved from environment")
-            
-        return domain_api_key
-
+    def load_configuration_from_file(self):
+    	try:
+    		app_config = json.loads("./config.jason")
+    	except Exception as e:
+    		logging.debug("Error loading configuration file. Exception: {}".format(e))
+    		return None
+    	
+    	return app_config
+    	
     def create_logger(self):
         logging.basicConfig(filename='dns-updater.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
